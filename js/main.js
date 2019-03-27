e6c2b184c2e4e3774114bbff2a21de479176f321
Asabeneh <asabeneh@gmail.com>
2019-03-27 11:55:44 +0200

    refactored to object

--- js/main.js
+++ js/main.js
@@ -1,56 +1,54 @@
-
-/*=== GLOBAL VARIABLES ===*/
-const countriesWrapper = document.querySelector ('.countries-wrapper');
-const subtitle = document.querySelector ('.subtitle');
-const searchInput = document.querySelector ('#search-input');
-const buttons = document.querySelector ('.buttons');
-
-buttons.addEventListener ('click', e => {
-  console.log (e.target.className);
-  alert (e.target.className);
-});
-
-/*=== Filter function === */
-const filterCountries = (arr, search) => {
-  const filteredCountries = arr.filter (country => {
-    let {name, capital, languages} = country;
-    let isName = name.toLowerCase ().includes (search);
-    let isCapital = capital.toLowerCase ().includes (search);
-    let isLanguages = languages.join ().toLowerCase ().includes (search);
-
-    return isName || isCapital || isLanguages;
-  });
-  let result = search == '' ? arr : filteredCountries;
-  return result;
-};
-
-/*=== Event listener to get search input === */
-
-searchInput.addEventListener ('input', e => {
-  let searchTerm = e.target.value.toLowerCase ();
-  showCountries (filterCountries (countries, searchTerm));
-});
-
-const createContent = content => {
-  const {name, capital, languages, population, flag} = content;
-  return `<div>
+const countriesList = {
+  elements: {
+    countriesWrapper: document.querySelector ('.countries-wrapper'),
+    subtitle: document.querySelector ('.subtitle'),
+    searchInput: document.querySelector ('#search-input'),
+    buttons: document.querySelector ('.buttons'),
+  },
+  createContent: function (content) {
+    const {name, capital, languages, population, flag} = content;
+    return `<div>
   <img src="${flag}" />
   <p>${name}</p>
   <p>${capital}</p>
   <p>${languages.join (', ')}</p>
   <p>${population.toLocaleString ()}</p>
 </div>`;
+  },
+  filterCountries: function (arr, search) {
+    const filteredCountries = arr.filter (country => {
+      let {name, capital, languages} = country;
+      let isName = name.toLowerCase ().includes (search);
+      let isCapital = capital.toLowerCase ().includes (search);
+      let isLanguages = languages.join ().toLowerCase ().includes (search);
+
+      return isName || isCapital || isLanguages;
+    });
+    let result = search == '' ? arr : filteredCountries;
+    return result;
+  },
+  showCountries: function (arr) {
+    let contents = '';
+    countriesWrapper.innerHTML = '';
+    arr.forEach ((country, i) => {
+      contents += countriesList.createContent (country);
+    });
+    countriesWrapper.innerHTML = contents;
+  },
 };
+
+let {countriesWrapper, subtitle, searchInput, buttons} = countriesList.elements;
+const {createContent, filterCountries, showCountries} = countriesList;
+buttons.addEventListener ('click', e => {
+  console.log (e.target.className);
+  alert (e.target.className);
+});
+
+/*=== Event listener to get search input === */
+searchInput.addEventListener ('input', e => {
+  let searchTerm = e.target.value.toLowerCase ();
+  showCountries (filterCountries (countries, searchTerm));
+});
 
 subtitle.textContent = `Currently, we have ${countries.length} countries`;
-
-const showCountries = arr => {
-  let contents = '';
-  countriesWrapper.innerHTML = '';
-  arr.forEach ((country, i) => {
-    contents += createContent (country);
-  });
-  countriesWrapper.innerHTML = contents;
-};
-
 showCountries (filterCountries (countries, searchInput.value));
